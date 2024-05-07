e0c5463332f3a19b9590935d5ce9bc92deb792f9
LukeJSimmons <lucasjsimmons@yahoo.com>
2024-05-07 09:47:54 -0500

    Add Header & Post Component

---
+++ src/components/Post.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Post() {
+    return (
+        <div>
+            <h2>Post</h2>
+            <img src="../images/NMELogo.png" />
+        </div>
+    );
+}
+
+export default Post;
