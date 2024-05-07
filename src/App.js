0136761459310d9d8ae374d6e1f72223332dd2ec
LukeJSimmons <lucasjsimmons@yahoo.com>
2024-05-07 11:17:00 -0500

    Add Post component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
 import './App.css';
 
+import { useState } from 'react';
+
 import Post from './components/Post';
 
+import image from './images/NMELogo.png';
+
 function App() {
+  const [votes, setVotes] = useState(0);
+
+  function increaseVotes() {
+    setVotes(votes+1);
+  }
+
+  function decreaseVotes() {
+    setVotes(votes-1);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1><em>Not-</em>Reddit</h1>
       </header>
-      <body>
-        <Post />
+      <body className="App-body">
+        <Post 
+          title={'title'}
+          image={image}
+          author={'author'}
+          date={'date'}
+          comments={'59'}
+          increaseVotes={increaseVotes}
+          decreaseVotes={decreaseVotes}
+          votes={votes}
+        />
       </body>
     </div>
   );
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,10 +1,34 @@
 import React from "react";
 
-function Post() {
+import './Post.css';
+
+function Post(props) {
+    function increase() {
+        props.increaseVotes();
+    }
+
+    function decrease() {
+        props.decreaseVotes();
+    }
+
     return (
-        <div>
-            <h2>Post</h2>
-            <img src="../images/NMELogo.png" />
+        <div className="Post">
+            <div id="post-contents">
+                <div id="votes">
+                    <button onClick={increase}>up</button>
+                    <p>{props.votes}</p>
+                    <button onClick={decrease}>down</button>
+                </div>
+                <div>
+                    <h2>{props.title}</h2>
+                    <img src={props.image} alt="" />
+                </div>
+            </div>
+            <div id="post-info">
+                <p>Posted by {props.author}</p>
+                <p>{props.date} Hours Ago</p>
+                <p>{props.comments}</p>
+            </div>
         </div>
     );
 }
